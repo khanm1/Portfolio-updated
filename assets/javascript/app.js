aa0a6d0fea449de8384a0a6db8696db9db08b278
mkhan <riponk9519@gamil.com>
2018-12-07 20:31:29 -0600

    Created javascript file

---
+++ assets/javascript/app.js
@@ -0,0 +1,100 @@
+$(document).ready(function () {
+    //functions for materialize
+    $(".dropdown-trigger").dropdown({
+        alignment: 'left',
+        coverTrigger: false,
+        inDuration: 200,
+        outDuration: 400
+    });
+    //populating content
+    function populateIntro() {
+        var introTitle = $("<p id='intro-headText'><span class='red-text'>Hi</span>!</p>");
+        var introContent = $("<p id='intro-text'>My name is <span class='red-text'>Mohammad</span>. <br>I'm a <span class='red-text'>full stack web developer</span>. <br>Welcome to my portfolio site!</p>");
+        var introDiv = $("<div class='content'></div>");
+        introDiv.attr("value", 1);
+        introDiv.append(introTitle).append(introContent);
+        return introDiv;
+    }
+
+    function populateAbout() {
+        var aboutTitle = $("<p id='about-headText'><span class='red-text'>About Me</span>:</p>");
+        var aboutContent = $("<p id='about-text'>I graduated from Boston, MA with a Bachelor's degree. While in school I was working full time as a Data Entry coordinator for Boston Scientific Corporation. After finishing school, I was promoted to corporate head quarter as Senior analyst. 2014 I moved to Chicago with a job at Allstate Insurance corporation as a lead QA analyst. </p>")
+        var aboutDiv = $("<div class='content'></div>");
+        aboutDiv.attr("value", 2);
+        aboutDiv.append(aboutTitle).append(aboutContent);
+        return aboutDiv;
+    }
+
+    function populateContact() {
+        var contactTitle = $("<p id='contact-headText' class='red-text'>Contact</p>");
+        var contactContent1 = $("<p class='contact-text' class='left-align'>Social Media: </p>");
+        var contactContent2 = $("<p class='contact-text' class='left-align' style='padding-bottom: 5%'>Email: <br><span class='red-text'><a href='mailto:khanrafika@yahoo.com'>khanrafika@yahoo.com</a></span></p>")
+        var imgrow = $("<div class='row'></div>");
+        var githubImgDiv=$("<div class='col s6'><a href='https://github.com/khanm1' target='_blank'><img class='responsive-img ourImage' src='assets/images/githublogo.jpg'></a></div>");
+        var linkedinImgDiv=$("<div class='col s6'><a href='https://www.linkedin.com/in/mohakhan' target='_blank'><img class='responsive-img ourImage' src='assets/images/linkedinlogo.jpg'></a></div>");
+        imgrow.append(githubImgDiv).append(linkedinImgDiv);
+        var contactDiv = $("<div class='content'></div>");
+        contactDiv.attr("value",4);
+        contactDiv.append(contactTitle).append(contactContent1).append(imgrow).append(contactContent2);
+        return contactDiv;
+    }
+
+    function populatePortfolio() {
+        var portfolioTitle = $("<p id='portfolio-headText' class='red-text'>Portfolio</p>");
+        //make cards from materialize card
+        // Need to change the Project name, Images and href link address
+        var projcard1 = $("<div class='col s12 m6 l4'><div class='card medium'><div class='card-image'><img src='assets/images/wordguessgame.png'><span style='font-size:1.40em;' class='card-title white-text red'>Word Guess Game</span></div><div class='card-content'><p class='card-textcontent'>Word Guess Game.</p></div><a style='font-size:1.40em;padding:10px;' class='red-text' href='https://khanm1.github.io/Word-Guess-Game/' target='_blank'>View Project</a><p style='font-size:1.40em;margin-top: 15px; margin-bottom: 5px;'><a class='black-text' href='https://github.com/khanm1/Word-Guess-Game' target='_blank'>GitHub</a></p></div></div>");
+        var projcard2 = $("<div class='col s12 m6 l4'><div class='card medium'><div class='card-image'><img src='assets/images/MTA.png'><span style='font-size:1.40em;' class='card-title white-text red'>Train Schedule App</span></div><div class='card-content'><p class='card-textcontent'>A web application to find the Train schedule and add train schedule using the Google Maps API. </p></div><a style='font-size:1.40em;padding:10px;' class='red-text' href='https://khanm1.github.io/train-schedule/' target='_blank'>View Project</a><p style='font-size:1.40em;margin-top: 15px; margin-bottom: 5px;'><a class='black-text' href='https://github.com/khanm1/train-schedule' target='_blank'>GitHub</a></p></div></div>");
+        var projcard3 = $("<div class='col s12 m6 l4'><div class='card medium'><div class='card-image'><img src='assets/images/darkness.png'><span style='font-size:1.40em;' class='card-title white-text red'>Giftastic Web App</span></div><div class='card-content'><p class='card-textcontent'>A web page using the GIPHY API to generate button categories which then generate gifs</p></div><a style='font-size:1.40em;padding:10px;' class='red-text' href='https://khanm1.github.io/giftastic/' target='_blank'>View Project</a><p style='font-size:1.40em;margin-top: 15px; margin-bottom: 5px;'><a class='black-text' href='https://github.com/khanm1/giftastic' target='_blank'>GitHub</a></p></div></div>");
+        var projcard4 = $("<div class='col s12 m6 l4'><div class='card medium'><div class='card-image'><img src='assets/images/australia.png'><span style='font-size:1.40em;' class='card-title white-text red'>Trivia Question Game</span></div><div class='card-content'><p class='card-textcontent'>A timed true and false trivia question game created using JavaScript timeout functions and CSS for design</p></div><a style='font-size:1.40em;padding:10px;' class='red-text' href='https://khanm1.github.io/TriviaGame/' target='_blank'>View Project</a><p style='font-size:1.40em;margin-top: 15px; margin-bottom: 5px;'><a class='black-text' href='https://github.com/khanm1/TriviaGame' target='_blank'>GitHub</a></p></div></div>");
+
+        var cardrow = $("<div class='row'></div>");
+        cardrow.append(projcard1).append(projcard2).append(projcard3);
+        var cardrow2 = $("<div class='row'></div>");
+        cardrow2.append(projcard4);
+        var portfolioDiv = $("<div class='content'></div>");
+        portfolioDiv.attr("value",3);
+        portfolioDiv.append(portfolioTitle).append(cardrow);
+        portfolioDiv.append(cardrow2);
+        return portfolioDiv;
+    }
+
+    function changeDiv(cValue,valueNew,newDiv){
+        if(cValue != valueNew){
+            $("div[value=" + cValue + "]").fadeOut(1000,function(){
+                newDiv.fadeIn(500);
+            });
+        }
+    }
+    var currentValue = 1;
+    var intro = populateIntro();
+    var about = populateAbout();
+    var portfolio = populatePortfolio();
+    var contact = populateContact();
+    $("#attachDiv").append(intro).append(about).append(portfolio).append(contact);
+    //when user first loads page, only show intro
+    
+    about.hide();
+    portfolio.hide();
+    contact.hide();
+    //click functions
+    $("#homePage").click(function(){
+        changeDiv(currentValue,1,intro);
+        currentValue = 1;
+    })
+    $("#topTitle").click(function(){
+        $("#homePage").trigger('click');
+    })
+    $("#aboutPage").click(function(){
+        changeDiv(currentValue,2,about);
+        currentValue = 2;
+    })
+    $("#portfolioPage").click(function(){
+        changeDiv(currentValue,3,portfolio);
+        currentValue = 3;
+    })
+    $("#contactPage").click(function(){
+        changeDiv(currentValue,4,contact);
+        currentValue = 4;
+    })
+})
